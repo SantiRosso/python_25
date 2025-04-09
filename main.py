bd9e303b4076a2a497e4d26ca8157f0c79c14092
Santiago Rosso <santiagorosso@live.com.ar>
2025-04-09 18:15:37 -0300

    hola mundo en python

---
+++ main.py
@@ -0,0 +1,11 @@
+from operacion import Operacion
+
+valorA=45
+valorB=10
+
+operacion= Operacion(valorA,valorB)
+
+print("La suma es: ",operacion.suma())
+print("La resta es: ",operacion.resta())
+print("La multiplicacion es: ",operacion.multiplicacion())
+print("La division es: ",operacion.division())  
